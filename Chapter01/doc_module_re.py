cac017371cc290e4acc672749e9d6e17f47a54bd
Warren Jitsing <warren.jitsing@gmail.com>
2020-07-13 16:19:46 +0200

    Start doc re module
    See: https://docs.python.org/3/library/re.html
    
    Why: Catalogue re module attributes and methods
    
    How: Python3 re
    
    Tags: re module

---
+++ Chapter01/doc_module_re.py
@@ -0,0 +1,28 @@
+"""
+re - Regular expression operations
+
+Provides regular expression matching operations similar to those found
+in Perl
+
+Patterns and strings to be searched can be Unicode strings (str) as well
+as 8-bit strings (bytes).
+    Unicode and 8-bit cannot be mixed
+
+Regular expressions use the backslash character ("\") to indicate special
+forms or to allow special characters to be used without invoking their
+special meaning. Use raw string notation to avoid
+
+Regular expression operations are available as module-level functions
+and methods on compiled regular expressions. The functions are shortcuts
+that don't require you to compile a regex object first, but miss some
+fine-tuning parameters.
+
+
+Regular Expression Syntax
+
+A regular expression specifies a set of strings that matches it; the
+functions in this module let you check if a particular string matches
+a given regular expression.
+
+
+"""
